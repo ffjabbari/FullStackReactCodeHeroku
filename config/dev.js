d62b147b54ece02c4062cc2cbb9cbe0b3584dc1e
gl3127 <fjabbari@ventera.com>
2021-06-14 18:51:01 -0500

    a4

--- config/dev.js
+++ config/dev.js
@@ -1,6 +1,6 @@
 module.exports = {
-  googleClientID: '303391505387-7nssssptda6n9ekbc7pegqtge61qq58t.apps.googleusercontent.com',
-  googleClientSecret: 'OM9kx-jhKJzBAdfKjrhCpch3',
+  googleClientID: '548271082756-krvo7o9n25mpfd71e3k2g7rahlk7olo4.apps.googleusercontent.com',
+  googleClientSecret: 'LNtorf92cZqqVtxAniyo2PLT',
   mongoURI: 'mongodb+srv://ffjabbari:15pishyy@emaily-dev.x7ckf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
   cookieKey: 'fredman'
 };
