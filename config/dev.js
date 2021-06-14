bdaf4017973f17bf6683db98514d3f64797aef74
gl3127 <fjabbari@ventera.com>
2021-06-14 18:11:31 -0500

    a2

---
+++ config/dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+  googleClientID: '303391505387-7nssssptda6n9ekbc7pegqtge61qq58t.apps.googleusercontent.com',
+  googleClientSecret: 'OM9kx-jhKJzBAdfKjrhCpch3',
+  mongoURI: 'mongodb+srv://ffjabbari:15pishyy@emaily-dev.x7ckf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  cookieKey: 'fredman'
+};
